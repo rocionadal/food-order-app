37826f1f569ee297a2d6d9774acb444d7ac43180
rocionadal <rocioonadal@gmail.com>
2022-02-26 20:27:24 +0100

    create basics for meals component

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,3 +1,13 @@
-const Meals = (props) => {};
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = (props) => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+};
 
 export default Meals;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        All our meals are cooked with high-quality ingredients by experienced
+        chefs!
+      </p>
+      <p>
+        Choose your favorite meal from our broad selection of available meals
+        and enjoy a delicious lunch or dinner at home.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;

19c57fa241b4200f435b26919047c9faba07eedb
rocionadal <rocioonadal@gmail.com>
2022-02-27 13:19:50 +0100

    passing ref with forwardRef

--- src/components/Meals/MealItem/MealIteamForm.js
+++ src/components/Meals/MealItem/MealIteamForm.js
@@ -1,10 +1,19 @@
+import { useRef } from 'react';
+
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label='Amount'
         input={{
           id: 'amount_' + props.id,

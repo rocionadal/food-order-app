199d18805ced8d4c9f5608d7c591674fc8020bc7
rocionadal <rocioonadal@gmail.com>
2022-03-16 21:16:38 +0100

    adding effect to cart when adding items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
@@ -6,14 +6,35 @@ import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [isBtnHighlighted, setIsBtnHighlighted] = useState(false);
   const ctx = useContext(CartContext);
+  const { items } = ctx;
 
-  const numberOfCartItem = ctx.items.reduce((curNumber, item) => {
+  const numberOfCartItem = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    isBtnHighlighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setIsBtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setIsBtnHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
